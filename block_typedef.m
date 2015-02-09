381f6a5230e47e1b98161cb1a1db0fabbe5d417b
Don Frehulfer <don@trello.com>
2015-02-09 10:31:17 -0500

    Imported user code snippets

---
+++ block_typedef.m
@@ -0,0 +1,10 @@
+// Block Typedef
+// 
+//
+// IDECodeSnippetCompletionPrefix: typedefBlock
+// IDECodeSnippetCompletionScopes: [All]
+// IDECodeSnippetIdentifier: 0703E404-3966-419C-88B8-F6463B3D567C
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C-Plus-Plus
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+typedef <#returnType#> (^<#typeName#>)(<#parameters#>);
---
+++ ignore_deprecations.m
@@ -0,0 +1,13 @@
+// Ignore Deprecations
+// 
+//
+// IDECodeSnippetCompletionPrefix: ignoreDep
+// IDECodeSnippetCompletionScopes: [CodeBlock]
+// IDECodeSnippetIdentifier: 39D35575-EAA9-4584-A370-944046D20839
+// IDECodeSnippetLanguage: Xcode.SourceCodeLanguage.Objective-C
+// IDECodeSnippetUserSnippet: 1
+// IDECodeSnippetVersion: 2
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+    <#code#>
+#pragma clang diagnostic pop
